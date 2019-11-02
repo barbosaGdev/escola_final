aeae77549f0f211ab800fc9a24e32fe39a742373
barbosaGdev <biki.jam@hotmail.com>
2019-11-02 15:55:52 -0300

    SGE EDUCACIONAL - Primeira Finalização (Testes)

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -5,11 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/css/padrao.css">
+    <link rel="stylesheet" href="/css/home.css">
     <link rel="stylesheet" href="/css/success.css">
     <title>Sistema de matricula</title>
 </head>
 <body>
     @include("header")
+    <a href="/" class="button-big darkred" >Voltar</a>
     <?php
         $title = "";
         $msg = "";
